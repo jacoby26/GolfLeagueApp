2c3d7449295439425caf39f0138fec15107953f7
Nate Lachs <Nate.J.Lachs@gmail.com>
2022-05-30 12:35:37 -0400

    dmt

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,3 +7,36 @@
     <router-view />
   </div>
 </template>
+<<<<<<< Updated upstream
+=======
+<script>
+export default {
+methods:{
+  toggleDark() {
+    if(document.querySelector('html').classList.contains('dark')){
+      document.querySelector('html').classList.remove('dark');
+    } else {
+      document.querySelector('html').classList.add('dark');
+    }
+  }
+}  
+}
+</script>
+<style>
+html.dark{
+  background-color: #0C003F;
+  color: #FFAF00;
+}
+html{
+  background-color: rgba(235, 255, 245, 64);
+  color: #6A0012;
+  text-align: center;
+}
+.dark a{
+  color: #FFAF00;
+}
+a{
+  color: #6A0012; 
+}
+</style>
+>>>>>>> Stashed changes
