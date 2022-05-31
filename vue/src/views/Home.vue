78f85387a19307160b36d8c5950c594e03bd1787
Nikolas Medina <njmedina@protonmail.com>
2022-05-31 17:52:01 -0500

    pushing map

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,19 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <google-map></google-map>
   </div>
+  
 </template>
 
+
 <script>
+import GoogleMap from "../components/GoogleMap.vue"
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    GoogleMap
+  }
 };
 </script>
