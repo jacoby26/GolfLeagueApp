e105c29161c10d6f2e8d30b0b024adf8f93db4d9
Nate Lachs <Nate.J.Lachs@gmail.com>
2022-05-31 19:59:55 -0400

    css changes

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,35 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" v-bind:class="{'logged-in': logged}">
+    <div id="nav" v-if="$store.state.token != ''">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;|&nbsp;
-      <button id="toggle dark" v-on:click.prevent="toggleDark()">Toggle Dark Mode</button>
+      <router-link v-bind:to="{ name: 'add-league' }">Create League</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'home' }">Manage League</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
-    
     <router-view />
+    <Messenger v-if="$store.state.token != ''"/>
   </div>
 </template>
 <script>
+import Messenger from "./components/Messenger.vue";
 export default {
-methods:{
-  toggleDark() {
-    if(document.querySelector('html').classList.contains('dark')){
-      document.querySelector('html').classList.remove('dark');
-    } else {
-      document.querySelector('html').classList.add('dark');
+  data() {
+    return{
     }
-  }
+  },
+  computed:{
+    logged(){
+      return this.$store.state.token != '';
+    }
+  },
+  components:{
+    Messenger
+  },
+methods:{
 }  
 }
 </script>
 <style>
-/* temporarily disabled to merge
-html.dark{
-  background-color: rgba(15, 0, 64, 64);
-  color: #FFAF00;
+#app.logged-in{
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  display: grid;
+  grid-template-columns: 75vw 25vw;
+  grid-template-rows: 10vh 90vh;
+  grid-template-areas: "nav nav"
+  "router messaging";
+}
+Messenger{
+  grid-area: messaging;
+}
+router-view{
+  grid-area: router;
+}
+
+#nav{
+ grid-area: nav;
+ position:static;
+ background-image: url("../Banner.png");
+ font-size: 14pt;
+ text-align: center;
+ font-family: Arial, Helvetica, sans-serif;
+ width: 1fr;
+ padding-top: 20px;
+ padding-bottom: 20px;
+ display: flex;
+ justify-content: space-around;
+ align-items: center;
 }
 html{
-  background-color: rgba(240, 255, 255, 64);
-  color: #6A0012;
-} */
+  background-color: #BAC7CA;
+  color: #ffb81f;
+}
+a{
+  color: #ffb81f; 
+}
 </style>
