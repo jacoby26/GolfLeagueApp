24563fe984a38fb9af086fe06c208fb614026018
Jacob Garside <jacoby26@gmail.com>
2022-06-06 12:46:10 -0400

    adding new round logic

--- vue/src/services/LeagueService.js
+++ vue/src/services/LeagueService.js
@@ -7,21 +7,21 @@ export default {
     viewStandings(leagueID){
         return axios.get(`/leagues/${leagueID}/scores`);
     },
-    viewRounds(user){
-        return axios.get('/rounds', user);
+    viewRounds(user, leagueId){
+        return axios.get(`/leagues/${leagueId}/rounds`, user, this.$store.league);
     },
-    addRound(user, round){
-        return axios.post('/rounds/addround', round.score, round.roundDate, user, round.golfCourse)
+    addRound(round, leagueId){
+        return axios.post(`/leagues/${leagueId}/addround`, round, this.$store.league)
     },
     addLeague(league){
         return axios.post('/leagues/addleague', league)
     },
     manageLeague(user, roster, leagueId){
-        return axios.put(`/leagues/${leagueId})`, user, roster)
+        return axios.put(`/leagues/${leagueId}`, user, roster)
     },
     reportRound(user, round, score){
         return axios.put(`/rounds/${round}`, score, user)
     },
-
+    
 
 }
