3db73694ea72ccb6077e0b14c6a2132fdbc8e683
Dillon Donoho <scopicaudio@gmail.com>
2022-06-07 16:51:14 -0400

    added backend for scores

---
+++ java/src/main/java/com/techelevator/dao/JdbcScoreDao.java
@@ -0,0 +1,40 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.GolfCourse;
+import com.techelevator.model.League;
+import com.techelevator.model.Score;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JdbcScoreDao implements ScoreDao{
+
+    @Autowired
+    JdbcScoreDao jdbcScoreDao;
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcScoreDao(DataSource ds) {
+        this.jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+    @Override
+    public long enterScore(Score score) {
+        String sql = "INSERT INTO scores (score, user_id, round_id) "
+                + "VALUES (?,?,?) RETURNING score_id";
+        return jdbcTemplate.queryForObject(sql, long.class, score.getScore(), score.getUser_id(), score.getRoundID());
+    }
+}
+
+
--- vue/src/components/CreateLeagueForm.vue
+++ vue/src/components/CreateLeagueForm.vue
@@ -25,7 +25,7 @@
         </div>
       </form>
 </div>
-      
+
 </template>
 
 <script>
